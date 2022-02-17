1ce12c2a0372156c9c476fca00552cbd97b23e3a
aketa <NewRepository@Deloitte>
2022-02-17 14:49:15 +0100

    Changed a lot of things

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String [] args){
-        System.out.println("Welcome to the souls of the damned");
+        System.out.println("Welcome to the souls of the damned!");
+        for (int i=0; i<=1234567;i++){
+if (i==0){
+    System.out.println((i+1)+" soul of the damned!");
+}else{
+    System.out.println((i+1)+" souls of the damned!")
+}
+        }
     }
 }
